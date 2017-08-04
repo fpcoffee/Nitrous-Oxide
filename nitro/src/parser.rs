599a25e14af339d1764fcacf7d9bfbd5b02a2c98
Jesse Maes <kebertyx@gmail.com>
2017-08-04 02:23:40 -0600

    Write demo parser for lambda terms

--- nitro/src/parser.rs
+++ nitro/src/parser.rs
@@ -1,15 +1,77 @@
-pub enum Lambda<'a> {
-    Variable { expr: String },
-    Function { var: String, expr: &'a Lambda<'a> },
-    Application { func: &'a Lambda<'a>, arg: &'a Lambda<'a> }
+use std::str;
+use nom::{IResult,alpha,space};
+
+#[derive(Debug, Clone)]
+pub enum Lambda {
+    Variable { var: String },
+    Function { var: String, expr: Box<Lambda> },
+    Application { func: Box<Lambda>, arg: Box<Lambda> }
 }
 
 pub fn show_lambda(term: &Lambda) -> String {
     match *term {
-        Lambda::Variable { ref expr } => expr.clone(),
+        Lambda::Variable { ref var } => var.clone(),
         Lambda::Function { ref var, ref expr } =>
             format!("(λ{}.{})", var, show_lambda(expr)),
         Lambda::Application { ref func, ref arg } =>
             format!("({} {})", show_lambda(func), show_lambda(arg)),
     }
+}
+
+pub fn read_lambda(input: &str) -> Option<Lambda> {
+    let result = lambda_term(input.as_bytes());
+
+    if result.is_done() {
+        Some(result.unwrap().1)
+    } else {
+        None
+    }
+}
+
+fn lambda_term(input: &[u8]) -> IResult<&[u8], Lambda> {
+    alt!(input, variable | function | application)
+}
+
+fn decode(input: &[u8]) -> String {
+    str::from_utf8(input).unwrap().to_string()
+}
+
+fn variable(input: &[u8]) -> IResult<&[u8], Lambda> {
+    do_parse!(input,
+        var: alpha >>
+        (Lambda::Variable {var: decode(var)})
+    )
+}
+
+fn function(input: &[u8]) -> IResult<&[u8], Lambda> {
+    delimited!(input,
+               tag!("("),
+               do_parse!(
+                   tag!("λ") >>
+                   var: alpha >>
+                   tag!(".") >>
+                   expr: lambda_term >>
+                   (Lambda::Function {
+                       var: decode(var),
+                       expr: Box::new(expr)
+                   })
+               ),
+               tag!(")")
+    )
+}
+
+fn application(input: &[u8]) -> IResult<&[u8], Lambda> {
+    delimited!(input,
+               tag!("("),
+               do_parse!(
+                   func: lambda_term >>
+                   call!(space) >>
+                   arg: lambda_term >>
+                   (Lambda::Application {
+                       func: Box::new(func),
+                       arg: Box::new(arg)
+                   })
+               ),
+               tag!(")")
+        )
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@ use nitro::parser;
 use nitro::parser::Lambda;
 
 fn main() {
-    let two = Lambda::Variable { expr: "2".to_string() };
-    let x = Lambda::Variable { expr: "x + x".to_string() };
-    let f = Lambda::Function { var: "x".to_string(), expr: &x };
-    let ap = Lambda::Application { func: &f, arg: &two};
-
-    // println!("{:?}", parser::show_lambda(two));
+    let xxyx = Lambda::Variable {var: "xxyx".to_string()};
+    let yf = Lambda::Function {var: "y".to_string(), expr: Box::new(xxyx)};
+    let xf = Lambda::Function {var: "x".to_string(), expr: Box::new(yf)};
+    let abc = Lambda::Variable {var: "abc".to_string()};
+    let ap = Lambda::Application {func: Box::new(xf), arg: Box::new(abc)};
     println!("{}", parser::show_lambda(&ap));
+
+    let term = "((λx.(λy.xxyx)) abc)";
+    println!("{}", term);
+
+    let parsed = parser::read_lambda(term);
+    println!("{}", parser::show_lambda(&parsed.unwrap()));
 }
