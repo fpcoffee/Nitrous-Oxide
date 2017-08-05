b326887141c8cd9e3ffd94fe54e110a1eb8503d5
Jesse Maes <kebertyx@gmail.com>
2017-08-05 10:24:09 -0600

    Add more checks for parsing chars and strings

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,28 @@ fn main() {
 
     for r in reals.iter() {
         println!("{}", r);
-        println!("{:?}\n", expr::parse_Real(r));
+        println!("{:?}\n", expr::parse_ConstExpr(r));
+    }
+
+    let chars = vec![
+        "'\\0'", "'\\a'", "'\\b'", "'\\t'", "'\\n'", "'\\v'", "'\\f'", "'\\r'",
+        "'\\u1337'", "'\\x42'", "'a'", "'s'", "'d'", "'f'", "'\x7f'"
+    ];
+
+    for c in chars.iter() {
+        println!("{}", c);
+        println!("{:?}\n", expr::parse_ConstExpr(c));
+    }
+
+    let strs = vec![
+        "\"\"", "\"hello\"",  "\"Good\\x42ye!\"", "\"\\\"Nevermore\\\"\"",
+        "\"\\u03bb\\u03c7\\u2192\\u03c7\\u00b2\"",
+        "\"Can a string be really long\\n\\tAnd have a new line and shit?\"",
+        "\"This should fail\nyo\"", "Escaping something I shouldn\'t"
+    ];
+
+    for s in strs.iter() {
+        println!("{}", s);
+        println!("{:?}\n", expr::parse_ConstExpr(s));
     }
 }
