2824260098de310b7addfd1f28417178e322c4e8
Jesse Maes <kebertyx@gmail.com>
2017-08-05 10:27:22 -0600

    Add more types for nitro patterns and expressions

--- nitro/src/parser/types.rs
+++ nitro/src/parser/types.rs
@@ -6,3 +6,52 @@ pub enum ConstExpr {
     Char(char),
     String(String)
 }
+
+pub enum Pattern {
+    Any,
+    Con,
+    Ident { id: String },
+    Tagged { pat: Box<Pattern> },
+    Record { fields: Vec<(String, Pattern)> },
+    Named {
+        pat: Box<Pattern>,
+        name: String
+    },
+}
+
+pub enum Expr {
+    Var { var: String },
+    New { expr: Box<Expr> },
+    Const { expr: ConstExpr },
+    Parens { expr: Box<Expr> },
+    Match { rules: Vec<(Pattern, Expr)> },
+    NewRecord { fields: Vec<(String, Expr)> },
+    Abstraction {
+        pat: Pattern,
+        arg: Box<Expr>
+    },
+    Application {
+        func: Box<Expr>,
+        arg: Box<Expr>
+    },
+    GetField {
+        expr: Box<Expr>,
+        name: String
+    },
+    SetField {
+        expr: Box<Expr>,
+        name: String,
+        value: Box<Expr>
+    },
+    Let {
+        pat: Pattern,
+        value: Box<Expr>,
+        expr: Box<Expr>
+    },
+    Recursion {
+        var: String,
+        pat: Pattern,
+        value: Box<Expr>,
+        expr: Box<Expr>
+    }
+}
