df85aba36a3b9fbae3fba3ab6afda7534426ae17
Jesse Maes <kebertyx@gmail.com>
2017-08-06 21:08:06 -0600

    Move test cases to tests/ directory

---
+++ nitro/tests/parser_expr.rs
@@ -0,0 +1,109 @@
+extern crate nitro;
+
+use nitro::parser::expr;
+use nitro::parser::types::ConstExpr;
+
+#[test]
+fn parse_reals() {
+    let valid = vec![
+        "inf", "-inf", "NaN", "-NaN", "0.123", "-0.123", "3.14", "-3.14", "0.",
+        "-0.", "5.", "-5.", "0.123e2", "0.456E4", "0.789e-8", "0.101112E-16",
+        "3.14e10", "-3.14e10", "0.e10", "-0.e10", "5.e10", "-5.e10", "123e45",
+        "678E9", "10e-11", "12E-14", "0e12"
+    ];
+
+    for r in valid.iter() {
+        let res = expr::parse_ConstExpr(r);
+
+        assert!(
+            match res {
+                Ok(ConstExpr::Real(_)) => true,
+                _ => false
+            },
+            format!("Cannot parse as real: \"{}\" -> {:?}", r, res)
+        );
+    }
+}
+
+
+#[test]
+fn parse_bad_reals() {
+    let invalid = vec![
+        // Commented out tests can be incompletely parsed?
+        "", ".", ".e10", "25", "3 .14", // "3..14", "3.14.15"
+    ];
+
+    for r in invalid.iter() {
+        let res = expr::parse_ConstExpr(r);
+
+        assert!(
+            match res {
+                Ok(ConstExpr::Real(_)) => false,
+                _ => true
+            },
+            format!("Erroneously parsed real: \"{}\"", r)
+        );
+    }
+}
+
+
+#[test]
+fn parse_chars() {
+    let valid = vec![
+        "'\\0'", "'\\a'", "'\\b'", "'\\t'", "'\\n'", "'\\v'", "'\\f'", "'\\r'",
+        "'\\u1337'", "'\\x42'", "'a'", "'s'", "'d'", "'f'"
+    ];
+
+    for c in valid.iter() {
+        let res = expr::parse_ConstExpr(c);
+        assert!(
+            match res {
+                Ok(ConstExpr::Char(_)) => true,
+                _ => false
+            },
+            format!("Cannot parse as char: \"{}\" -> {:?}", c, res)
+        );
+    }
+}
+
+
+#[test]
+fn parse_strings() {
+    let valid = vec![
+        "\"\"", "\"hello\"",  "\"Good\\x42ye!\"", "\"\\\"Nevermore\\\"\"",
+        "\"\\u03bb\\u03c7 \\u2192 \\u03c7\\u00b2\"",
+        "\"Can a string be really long\\n\\tAnd have a new line and shit?\"",
+    ];
+
+    for s in valid.iter() {
+        let res = expr::parse_ConstExpr(s);
+
+        assert!(
+            match res {
+                Ok(ConstExpr::String(_)) => true,
+                _ => false
+            },
+            format!("Cannot parse as char: \"{}\" -> {:?}", s, res)
+        );
+    }
+}
+
+
+#[test]
+fn parse_bad_strings() {
+    let invalid = vec![
+        "\"This should fail\nyo\"", "\"Escaping something I shouldn\\'t\""
+    ];
+
+    for s in invalid.iter() {
+        let res = expr::parse_ConstExpr(s);
+
+        assert!(
+            match res {
+                Ok(ConstExpr::String(_)) => false,
+                _ => true
+            },
+            format!("Cannot parse as char: \"{}\" -> {:?}", s, res)
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,5 @@ extern crate nitro;
 use nitro::parser::expr;
 
 fn main() {
-    let reals = vec![
-        "3.14", "-3.14", "2.5E10", "2.5e10", "2.5E-10", "5.", ".5", "0.5",
-        "inf", "-inf", "NaN", "."
-    ];
-
-    for r in reals.iter() {
-        println!("{}", r);
-        println!("{:?}\n", expr::parse_ConstExpr(r));
-    }
-
-    let chars = vec![
-        "'\\0'", "'\\a'", "'\\b'", "'\\t'", "'\\n'", "'\\v'", "'\\f'", "'\\r'",
-        "'\\u1337'", "'\\x42'", "'a'", "'s'", "'d'", "'f'", "'\x7f'"
-    ];
-
-    for c in chars.iter() {
-        println!("{}", c);
-        println!("{:?}\n", expr::parse_ConstExpr(c));
-    }
-
-    let strs = vec![
-        "\"\"", "\"hello\"",  "\"Good\\x42ye!\"", "\"\\\"Nevermore\\\"\"",
-        "\"\\u03bb\\u03c7 \\u2192 \\u03c7\\u00b2\"",
-        "\"Can a string be really long\\n\\tAnd have a new line and shit?\"",
-        "\"This should fail\nyo\"", "\"Escaping something I shouldn\\'t\""
-    ];
-
-    for s in strs.iter() {
-        println!("{}", s);
-        println!("{:?}\n", expr::parse_ConstExpr(s));
-    }
+    println!("ERROR: Compiler not yet written!");
 }
