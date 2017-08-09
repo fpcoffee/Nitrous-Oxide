c95b67ea8071785f4526a35acdbd6260b670cc8d
Jesse Maes <kebertyx@gmail.com>
2017-08-08 18:49:53 -0600

    Add a neat REPL

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 extern crate nitro;
 
+use std::io;
+use std::io::Write;
+use std::io::BufRead;
+use std::io::BufReader;
 use nitro::parser::expr;
 
 fn main() {
-    println!("ERROR: Compiler not yet written!");
+    let stdin = BufReader::new(io::stdin());
+
+    print!("nitro>>> ");
+    let _ = io::stdout().flush();
+
+    for line in stdin.lines() {
+        let input = line.unwrap();
+        let res = expr::parse_Expr(&input[..]);
+
+        println!("{:#?}\n", res);
+        print!("nitro>>> ");
+        let _ = io::stdout().flush();
+    }
 }
